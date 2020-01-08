ab87c1fb41b593c140ca4a6793308b0c1e71577a
Abdi <oxabdi75@gmail.com>
2020-01-08 09:23:45 +0300

    added navbar aljazeera html

--- app.py
+++ app.py
@@ -60,6 +60,27 @@ def bbc():
  
     return render_template('bbc.htm', context=mylist)
  
+@app.route('/Aljzeera')
+def Aljzeera():
+    newsapi = NewsApiClient(api_key="6788abcbf60f48dc9cee181b96f275d3")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+    mylist = zip(news, desc, img)
+ 
+    return render_template('Aljzeera.htm', context=mylist)
  
  
 if __name__ == "__main__":
